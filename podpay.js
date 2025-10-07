63a9fe2b8683db31881ac3b6bd505b22d7af9b07
anajoliva30-del <anajoliva30@gmail.com>
2025-10-07 19:54:41 -0300

    podpay.js

---
+++ podpay.js
@@ -0,0 +1,19 @@
+import fetch from 'node-fetch';
+
+export default async function handler(req, res) {
+  const response = await fetch('https://api.podpay.com.br/v1/payments', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer sk_2rjshg0u3wAC9sms4YuzBxWjRqabCsJx9ril'
+    },
+    body: JSON.stringify({
+      amount: 1000,
+      description: "Teste de pagamento",
+      callback_url: "https://seusite.github.io"
+    })
+  });
+
+  const data = await response.json();
+  res.status(200).json({ paymentUrl: data.payment_url });
+}
